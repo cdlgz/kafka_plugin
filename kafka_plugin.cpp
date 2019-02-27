612bc109804e93105bf776490750acf5fb89ac8e
zhizhong <deng.zhizhong@shuweikeji.com>
2019-02-27 15:30:41 +0800

    update trx_id_str

--- kafka_plugin.cpp
+++ kafka_plugin.cpp
@@ -500,7 +500,7 @@ void kafka_plugin_impl::_process_applied_transaction(const trasaction_info_st &t
   const auto trx_id_str = trx_id.str();
   uint64_t time = (t.block_time.time_since_epoch().count() / 1000);
   string transaction_metadata_json =
-      "{\"trx_id\":\"" + std::to_string(trx_id_str) + "\",\"block_number\":" + std::to_string(t.block_number) + ",\"block_time\":" + std::to_string(time) +
+      "{\"trx_id\":\"" + trx_id_str + "\",\"block_number\":" + std::to_string(t.block_number) + ",\"block_time\":" + std::to_string(time) +
       ",\"trace\":" + fc::json::to_string(t.trace).c_str() + "}";
   producer->trx_kafka_sendmsg(KAFKA_TRX_APPLIED, (char *)transaction_metadata_json.c_str());
 }
